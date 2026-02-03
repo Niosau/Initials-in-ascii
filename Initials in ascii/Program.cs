c77453eb215f652028bbf1dff0293bedc2ac6de1
362704579 <362704579@2260I-R31-002>
2026-02-03 13:20:07 -0500

    Add project files.

---
+++ Initials in ascii/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Initials_in_ascii
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Did you know that if you type "ASCII art" into ChatGPT, it will generate ASCII art for you?
+            Console.Write(" C C C ");
+            Console.WriteLine("   L");
+            Console.Write("C     C");
+            Console.WriteLine("  L");
+            Console.Write("C     ");
+            Console.WriteLine("  L");
+            Console.WriteLine("C  ");
+        }
+    }
+}
