616b92bb0b154f5c45529436f2ab142a4cf2f9af
Cameron Link <362704579@GoTVDSB.ca>
2026-02-04 12:32:06 -0500

    Colour Added

--- Initials in ascii/Program.cs
+++ Initials in ascii/Program.cs
@@ -4,18 +4,35 @@
     {
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Displaying my initials in ASCII art...");
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Magenta;
             //did you know that if you type "ASCII art" into ChatGPT, it will generate ASCII art for you?
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.Write(" C C C ");
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("  L");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.Write("C     C");
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("  L");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.Write("C    ");
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("    L");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.Write("C    ");
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("    L");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.Write("C     C");
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("  L");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.Write(" C C C ");
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("  L L L L L");
         }
     }
