86fc99cfbe6dd0e166042c31c479b571954b7397
Cameron Link <362704579@GoTVDSB.ca>
2026-02-04 12:18:51 -0500

    finished it yipee

--- Initials in ascii/Program.cs
+++ Initials in ascii/Program.cs
@@ -4,14 +4,19 @@
     {
         static void Main(string[] args)
         {
-            //Did you know that if you type "ASCII art" into ChatGPT, it will generate ASCII art for you?
+            //did you know that if you type "ASCII art" into ChatGPT, it will generate ASCII art for you?
             Console.Write(" C C C ");
-            Console.WriteLine("   L");
+            Console.WriteLine("  L");
             Console.Write("C     C");
             Console.WriteLine("  L");
-            Console.Write("C     ");
+            Console.Write("C    ");
+            Console.WriteLine("    L");
+            Console.Write("C    ");
+            Console.WriteLine("    L");
+            Console.Write("C     C");
             Console.WriteLine("  L");
-            Console.WriteLine("C  ");
+            Console.Write(" C C C ");
+            Console.WriteLine("  L L L L L");
         }
     }
 }
