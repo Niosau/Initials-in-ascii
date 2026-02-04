6c85d34ccbdac07d1dddd27fad687929fd1ebe2c
Cameron Link <362704579@GoTVDSB.ca>
2026-02-04 12:40:01 -0500

    Fully finished i think

--- Initials in ascii/Program.cs
+++ Initials in ascii/Program.cs
@@ -4,11 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.BackgroundColor = ConsoleColor.Yellow;
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Displaying my initials in ASCII art...");
             Console.Clear();
-            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Hello, my name is Cameron");
+            Console.WriteLine();
+            Console.WriteLine("Comments are used to help you remember what your code does or to help others understand your code. It makes your code easier to read!");
+            Console.WriteLine();
             //did you know that if you type "ASCII art" into ChatGPT, it will generate ASCII art for you?
             Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.Write(" C C C ");
